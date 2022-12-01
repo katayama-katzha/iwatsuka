16b977f621cd70f171ae44d4c25da81bbecfe51b
TAKAHITO KATAYAMA <katzha.work@gmail.com>
2022-12-01 16:31:41 +0900

    qualityページ作成（pc/sp）

---
+++ assets/js/quality.js
@@ -0,0 +1,19 @@
+
+
+jQuery(function($){
+  
+
+  $(function(){
+    if (window.matchMedia('(max-width: 767px)').matches){
+      $('.sp200').attr('data-wow-offset', '200');
+    }else{
+      
+    };
+   
+
+  }());
+
+
+
+
+});
